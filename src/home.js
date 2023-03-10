c7a9a9e9e06b75243a7c7891ac26d086f520959f
WavyBoyJodii <najeecasey@gmail.com>
2023-03-10 10:30:01 -0500

    finish home page with no functionality must complete other pages and add event listeners to buttons to enable tabbed browsing

--- src/home.js
+++ src/home.js
@@ -45,10 +45,61 @@ export default function initHome() {
   squigLine.src = squig;
   squigLine.classList.add('name-art');
 
+  const splashMessage = document.createElement('div');
+  splashMessage.classList.add('splashMessage');
+
+  const message1 = document.createElement('div');
+  message1.textContent = `Chicken So Tasty`;
+
+  const message2 = document.createElement('div');
+  message2.textContent = `Chicken So Tender`;
+  message2.style.color = '#ed3c20';
+
+  const message3 = document.createElement('div');
+  message3.textContent = `Jodii's Wings`;
+
+  splashMessage.append(message1, message2, message3);
+
+  const smallSplash = document.createElement('p');
+  smallSplash.textContent =
+    "A world of delectable treasures awaits you. Welcome to Jodii's Wings, the premier Northen/Southern fusion chicken restaurant in NYC. Having worked in several 5 star restaurants Jodii is combining high class dining with the simple pleasures of chicken wings rated the best wing restaurnt by TimeOut magazine, we are here to please!";
+  smallSplash.classList.add('smallSplash');
+
+  const orderButton = document.createElement('button');
+  orderButton.textContent = 'Order Here';
+  orderButton.classList.add('orderButton');
+
+  const socialNavDiv = document.createElement('div');
+  socialNavDiv.classList.add('social-nav');
+
+  const instaNav = document.createElement('p');
+  instaNav.textContent = 'Instagram';
+  instaNav.classList.add('social-nav-button');
+
+  const faceNav = document.createElement('p');
+  faceNav.textContent = 'Facebook';
+  faceNav.classList.add('social-nav-button');
+
+  const twitNav = document.createElement('p');
+  twitNav.textContent = 'Twitter';
+  twitNav.classList.add('social-nav-button');
+
+  socialNavDiv.append(instaNav, faceNav, twitNav);
+
   frame.id = 'case';
   mainPage.classList.add('main');
 
-  mainPage.append(bizLogo, navDiv, mainPhoto, bigName, squigLine);
+  mainPage.append(
+    bizLogo,
+    navDiv,
+    mainPhoto,
+    bigName,
+    squigLine,
+    splashMessage,
+    smallSplash,
+    orderButton,
+    socialNavDiv
+  );
   frame.appendChild(mainPage);
   contentDiv.appendChild(frame);
 }
